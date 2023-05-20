4ece6c418c0a6cfc5a80309539839c5e1d3681c2
nikhilGithub5 <nikhil.hiremath1marist@gmail.com>
2023-05-20 18:21:10 -0400

    change in build config to decrease size of image - basically cutting down gradle from final image

--- checkout-orchestrator/src/main/java/com/nikhil/containers/checkoutorchestrator/api/AllCardsApi.java
+++ checkout-orchestrator/src/main/java/com/nikhil/containers/checkoutorchestrator/api/AllCardsApi.java
@@ -4,8 +4,11 @@ import com.nikhil.containers.checkoutorchestrator.model.Card;
 import com.nikhil.containers.checkoutorchestrator.service.CardsHandler;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +19,26 @@ public class AllCardsApi {
 
     @Autowired
     private CardsHandler cardsHandler;
+
+    @CrossOrigin
+    @GetMapping(value = "/test")
+    @ResponseBody
+    public ResponseEntity<String> getApp() {
+        String html = "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<body>\n" +
+                "\n" +
+                "<h1>FRONT END CONTAINER</h1>\n" +
+                "\n" +
+                "<p>CHECKOUT ORCHESTRATOR</p>\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>\n" +
+                "\n";
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(html);
+    }
 
     @GetMapping(value = "/card/credit")
     public ResponseEntity<List<Card>> getCards() {
--- payment-data-services/src/main/java/com/nikhil/containers/paymentdataservices/api/CardsApi.java
+++ payment-data-services/src/main/java/com/nikhil/containers/paymentdataservices/api/CardsApi.java
@@ -33,9 +33,9 @@ public class CardsApi {
                 "<html>\n" +
                 "<body>\n" +
                 "\n" +
-                "<h1>My First Heading</h1>\n" +
+                "<h1>BACK END CONTAINER</h1>\n" +
                 "\n" +
-                "<p>My first paragraph.</p>\n" +
+                "<p>PAYMENT DATA SERVICES</p>\n" +
                 "\n" +
                 "</body>\n" +
                 "</html>\n" +
